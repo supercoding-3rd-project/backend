6702e30e0dd9f16373013b301f2f7b642a3355ca
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-03-26 14:57:35 +0900

    .

---
+++ src/main/java/com/github/devsns/DevSnsApplication.java
@@ -0,0 +1,13 @@
+package com.github.devsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevSnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/devsns/DevSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
