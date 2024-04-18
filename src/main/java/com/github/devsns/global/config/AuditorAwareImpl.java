f129242c3837a38f813a2340138e1bf4d51f1672
yoonipark <yoonipark09@gmail.com>
2024-04-18 23:39:23 +0900

    추가 업데이트_0418 2nd

--- src/main/java/com/github/devsns/global/config/AuditorAwareImpl.java
+++ src/main/java/com/github/devsns/global/config/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 import java.util.Optional;
 @Component
 public class AuditorAwareImpl implements AuditorAware<String> {
--- src/main/java/com/github/devsns/global/config/WebConfiguration.java
+++ src/main/java/com/github/devsns/global/config/WebConfiguration.java
@@ -1,7 +1,7 @@
 package com.github.devsns.global.config;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
