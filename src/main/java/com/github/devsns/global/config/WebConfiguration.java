0a1203c73049ab38c264488b3d4446d7a312c637
yoonipark <yoonipark09@gmail.com>
2024-04-19 02:42:30 +0900

    CORS 주소 변경

--- src/main/java/com/github/devsns/global/config/WebConfiguration.java
+++ src/main/java/com/github/devsns/global/config/WebConfiguration.java
@@ -17,7 +17,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // 로컬 프런트엔드 서버 주소
+                .allowedOrigins("http://www.alco4dev.com")  // 로컬 프런트엔드 서버 주소
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PATCH.name(), HttpMethod.PUT.name(), HttpMethod.OPTIONS.name())
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 크레덴셜 허용
